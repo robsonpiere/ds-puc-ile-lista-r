02da0a5b6fad704b184fee4990a9fdbfb07b2878
Robson Henrique Piere Oliveira <robsonpiere@sescmg.com.br>
2017-04-25 14:25:35 -0300

    Criado arquivo 1.3

---
+++ Lista_1/Exercicio_1.2.R
@@ -0,0 +1,7 @@
+#LISTA DE EXERCICIOS 1 - INTRODUÇÃO
+#Vetores
+
+
+# 1.2 - O volume de um cone de raio R e altura H é dado por 13 ×  × R2 × H. Tendo obtido o volume e a altura de seis cones, apresentados a seguir, calcule o volume de cada um deles.
+#R = {2.27, 1.98, 1.69, 1.88, 1.64, 2.14}
+#H = {8.28, 8.04, 9.06, 8.70, 7.58, 8.34}
---
+++ Lista_1/Exercicio_1.3.R
@@ -0,0 +1,24 @@
+#LISTA DE EXERCICIOS 1 - INTRODUÇÃO
+#Operações matemáticas
+
+#1.3 - Calcule o que se pede:
+
+#a) 3 + 9/5
+
+#b) 56^4 + 29/5
+
+#c) raiz quadrada (p76)
+
+#d) ln 12345
+
+#e) log2 12345
+
+#f) log10 12345
+
+#g) C(6 2) - combinatório de seis, dois a dois (Dica: existe uma função que calcula isso).
+
+#h) |18 − 50|
+
+#i) sin(pi ), cos(pi ), tan(pi )
+
+#j) Arredonde o valor de  (pi) para 4 casas decimais;
