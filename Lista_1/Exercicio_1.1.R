28fe1c6cfe4ae5ce3fbb0d10d55e93c4e0a59133
Robson Piere <robsonpiere@outlook.com>
2017-04-25 13:28:54 -0300

    Projeto criado, exercicio 1 iniciado

--- Lista_1/Exercicio_1.1.R
+++ Lista_1/Exercicio_1.1.R
@@ -1,5 +1,31 @@
-#1.1
-vec <- c(26,NA,72,45,12,6,NA,88)
+#LISTA DE EXERCICIOS 1 - INTRODUÇÃO
+#Vetores
+#
 
-#a
+# 1.1 - Crie um vetor chamado vec contendo os elementos {26, NA, 72, 45, 12, 16, NA, 88} e obtenha o que se pede:
+vec <- c(26,NA,72,45,12,6,NA,88)
+vec
+
+# a) Obtenha um novo vetor (vec1) contendo o primeiro e o último elementos do vetor vec;
 vec1 <- c(head(vec,1),tail(vec,1))
+vec1
+
+# b) Insira entre os elementos 72 e 45 do vetor vec os elementos {24, 13, 42}. Dica: a função append()
+append(vec,c(24,13,42),match(72,vec))
+vec
+
+# c) Obtenha o produto dos elementos do vetor vec;
+prod(na.RM = TRUE, vec)
+# d) Obtenha os valores que não sejam NA;
+
+# e) Obtenha os valores maiores que a média dos elementos do vetor vec.
+
+# f) Obtenha um vetor contendo os valores que imediatamente antecedem e sucedem a cada NA. Dica: a função which() pode ser útil;
+
+# g) Dados os vetores a <- c(14, 27, 45, 72, 19, -12) e b <- c(6, 9, 8, NA, 7, 5), calcule a + b + 5;
+
+# h) Crie um vetor num_pares contendo os números inteiros pares menores que 100;
+
+# i) Exclua deste vetor os valores maiores que 60 e menores que 80;
+
+# j) Encontre a variância dos valores remanescentes.
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 #Lista de Exercícios R
+
+Lista de exercícios para o curso de pós graduação em Ciência de dados e Big Data - Puc Minas
