52df05287ab76cab9640a007aef8019b58bb3380
Robson Piere <robsonpiere@outlook.com>
2017-05-08 00:33:57 -0300

    ex 1.3 ok

--- Lista_1/Exercicio_1.3.R
+++ Lista_1/Exercicio_1.3.R
@@ -4,21 +4,31 @@
 #1.3 - Calcule o que se pede:
 
 #a) 3 + 9/5
+a <- 3 + 9/5
 
 #b) 56^4 + 29/5
+b <- 56^4 + 29/5
 
-#c) raiz quadrada (p76)
+#c) raiz quadrada (76)
+c = sqrt(76)
 
 #d) ln 12345
+d <-log(1)
 
 #e) log2 12345
+e <- log2(12345)
 
 #f) log10 12345
+f <-log10(12345)
 
 #g) C(6 2) - combinatório de seis, dois a dois (Dica: existe uma função que calcula isso).
+g <- choose(6,2)
 
 #h) |18 − 50|
+h <- abs(18 - 50)
 
 #i) sin(pi ), cos(pi ), tan(pi )
+i <- c(sin(pi),cos(pi),tan(pi))
 
 #j) Arredonde o valor de  (pi) para 4 casas decimais;
+j <- round(pi,4)
