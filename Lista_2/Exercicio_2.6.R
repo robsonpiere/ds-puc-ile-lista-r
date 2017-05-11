32cce325994d75d795f859b831385f0654f96186
Robson Henrique Piere Oliveira <robsonpiere@sescmg.com.br>
2017-05-11 08:55:00 -0300

    Lista 3 completa

--- Lista_2/Exercicio_2.6.R
+++ Lista_2/Exercicio_2.6.R
@@ -1,4 +1,12 @@
 # 
 # 2.6 - O R dispõe dos conjuntos de dados rock e ToothGrowth. Exporte estes dados para um arquivo do
 # Excel, de forma que cada conjunto de dados fique em uma planilha diferente.
-# 
+#
+library('datasets')
+install.packages('xlsx')
+library('xlsx')
+dados_rock = rock
+dados_ToothGrowth = ToothGrowth
+
+write.xlsx(dados_rock, "exercicio_2.6.xlsx", sheetName='rock')
+write.xlsx(dados_ToothGrowth, "exercicio_2.6.xlsx", sheetName='ToothGrowth', append=T)
--- Lista_3/Exercicio_3.R
+++ Lista_3/Exercicio_3.R
@@ -17,3 +17,59 @@
 # 3.16 - Obtenha as linhas 4, 10 e 19 e as colunas mpg, drat e vs do conjunto de dados;
 # 3.17 - Obtenha um vetor que seja a soma da variável cyl com a variável qtd_carburadoes. Qual é a soma
 # dos valores deste novo vetor?
+
+3.1
+library('datasets')
+dados = mtcars
+
+3.2
+nomes <- names(dados)
+
+3.3
+colunas <- ncol(dados)
+linhas <- nrow(dados)
+
+3.4
+media = mean(dados$mpg)
+
+3.5
+maximo <- max(dados$disp)
+minimo <- min(dados$disp)
+
+3.6
+correlacao <- cor(mtcars$mpg, mtcars$wt)
+
+3.7
+registros_3_7 <-mtcars[which(mtcars$mpg > 20 & mtcars$hp > 90),]
+
+3.8
+pos_carb <- which(names(dados)=="carb")
+names(dados)[pos_carb] <-"qtd_carburadores"
+
+3.9
+dados$qsec = NULL
+
+3.10
+dados_novo = dados[,c('mpg', 'wt', 'cyl')]
+
+3.11
+dados_novo = dados_novo[,c('cyl', 'mpg', 'wt')]
+
+3.12
+nome_linhas = row.names(dados)
+
+3.13
+valor_hp_15 = dados$hp[15]
+
+3.14
+media_mpg <- mean(dados[which(dados$cyl ==6),'mpg'])
+
+3.15
+produto <- max(dados$cyl) * min(dados$wt)
+
+3.16
+dados_3_16 <- dados[c(4,10,19), c('mpg','drat','vs')]
+
+3.17
+vetor_soma <- sum(dados$cyl + dados$qtd_carburadores)
+vetor_soma
